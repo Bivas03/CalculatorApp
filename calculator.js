f40721be5342aae6d4c118c90caedeba2ea540bf
Bivas03 <83751814+Bivas03@users.noreply.github.com>
2021-07-14 23:13:37 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,140 +1,65 @@
-class Calculator{
-    constructor(){
-        this.numArr = []
-        this.currentNum = ""
-        this.result = ""
-        this.prevResult = ""
-        this.operation = []
+var currentNum="";
+var result=0;
+var displayText="";
+var previousOperator="";
+var display = document.getElementById("input");
+
+const numKeyClickHandler = (num) => {
+    currentNum +=num;
+    displayText+=num;
+    display.value=displayText;
+}
+
+const operatorKeyClickHandler = (operator) => {
+    displayText += operator;
+    display.value=displayText;
+    if(previousOperator ===""){
+        result += Number(currentNum);
     }
-    add(numOne,numTwo){
-        return Number(numOne) + Number(numTwo)
-    }
-    subtract(numOne,numTwo){
-        return Number(numOne) -  Number(numTwo)
-    }
-    setCurrentNum(value){
-        this.currentNum += value
-    }
-    clearCurrentNum(){
-        this.numArr.push(this.currentNum) 
-        this.currentNum = ""
-    }
-    setOperation(operation){
-        this.operation.push(operation)
-        this.numArr.push(this.currentNum)
-        this.currentNum = ""
-    }
-    clearCalculator(){
-        this.numArr = []
-        this.currentNum = ""
-        this.result = ""
-        this.prevResult = ""
-        this.operation = []
-    }
-    changeSign(forNumOne){
-        if(forNumOne){
-            this.numOne = String(Number(this.numOne) * -1)
-            return
+    else{
+        if(previousOperator==="+"){
+            result += Number(currentNum);
         }
-        this.numTwo =  String(Number(this.numTwo) * -1)
-    }
-    undo(forNumOne){
-        if(forNumOne){
-            this.numOne =  this.numOne.substring(0,(this.numOne.length-1))
-            return;
+        else if(previousOperator==="-"){
+            result -= Number(currentNum);
         }
-        this.numTwo =  this.numTwo.substring(0,(this.numTwo.length-1))
-
-    }
-}
-
-const SignObject = {
-    ADD:"+",
-    SUBTRACT:"-",
-    MULTIPLY:"x",
-    DIVIDE:"÷"
-}
-
-
-
-
-const calculator = new Calculator()
-
-
-
-const updateMainDisplay = () =>{
-    const main = document.getElementById("main-display")
-    main.innerText =  calculator.currentNum || 0
-   
-}
-
-const updateSecondaryDisplay = () =>{
-    const secondary = document.getElementById("secondary-display")
-    const newNumArr = [...calculator.numArr]
-    const newOperation = [...calculator.operation]
-    let innerHTML = ""
-    while(newNumArr.length !== 0){
-        const num = newNumArr.shift()
-        const operation = newOperation.shift()
-        innerHTML += `${num} <span class="operand">${SignObject[operation] ? SignObject[operation] : ""}</span>`
-    }
-    secondary.innerHTML = innerHTML
-} 
-const numKeyClickHandler = (num) =>{
-    calculator.setCurrentNum(num)
-    updateMainDisplay()
-    updateSecondaryDisplay()
-}
-
-const operandKeyClickHandler = (operation) =>{
-    calculator.setOperation(operation)
-    updateMainDisplay()
-    updateSecondaryDisplay()
-}
-
-const resultClickHandler = () =>{
-    calculator.clearCurrentNum()
-    const newNumArr = [...calculator.numArr]
-    const newOperation = [...calculator.operation]
-    // TODO: Move logic to class as result method
-    while(newNumArr.length !== 1){
-        const numOne = newNumArr.shift()
-        const numTwo = newNumArr.shift()
-        const operation = newOperation.shift()
-
-        switch (operation) {
-            case "ADD":
-                newNumArr.unshift(calculator.add(numOne,numTwo))
-                break;
-            case "SUBTRACT":
-                newNumArr.unshift(calculator.subtract(numOne,numTwo))
-                break;
-            default:
-                break;
+        else if(previousOperator==="*"){
+            result *= Number(currentNum);
+        }
+         else if(previousOperator==="/"){
+             if(Number(currentNum)==0 && result==0){
+                  result = "NaN";
+             }else if(Number(currentNum)==0 ){
+                 result = 'Infinity';
+             }else{
+                  result /= Number(currentNum);
+             }
+            
         }
     }
-    updateSecondaryDisplay()
-    const main = document.getElementById("main-display")
-    main.innerText = newNumArr[0]
+    previousOperator = operator;
+    currentNum = "";
 }
 
-const actionKeyClickHandler = (action) => {
-    switch (action) {
-        case "CLEAR_ALL":
-            calculator.clearCalculator()
-            break;
-        case "CHANGE_SIGN":
-            calculator.changeSign(calculator.operation ? false : true)
-            break;
-        default:
-            break;
+const resultKeyClickHandler = () => {
+    if(currentNum===""){
+        display.value = String(result);
     }
-    updateMainDisplay()
-    updateSecondaryDisplay()
+    else{
+        operatorKeyClickHandler("=");
+        display.value = String(result);
+
+    }
+    currentNum="";
+    previousOperator=""
+    displayText=String(result);
 }
 
-const undoClickHandler = () =>{
-    calculator.undo(calculator.operation ? false : true)
-    updateMainDisplay()
-    updateSecondaryDisplay()
+const resetKeyClickHandler =()=>{
+    display.value = "0";
+    currentNum="";
+    result=0;
+    displayText="";
+    previousOperator="";
+
 }
